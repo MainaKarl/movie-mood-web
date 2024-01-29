e6018e653359318532da6f03211fef0062c6ae56
MainaKarl <mainakarlg@gmail.com>
2024-01-30 01:50:38 +0300

    Movie Trailer

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { BannerComponent } from '../../core/components/banner/banner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent, BannerComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
