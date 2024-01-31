9d170f3e64a8ee4b46022fb8121c6ef624e7cd3b
MainaKarl <mainakarlg@gmail.com>
2024-01-31 12:31:01 +0300

    Update

--- src/app/shared/components/movie-carousel/movie-carousel.component.ts
+++ src/app/shared/components/movie-carousel/movie-carousel.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 
 
@@ -11,12 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './movie-carousel.component.scss',
 
 })
-export class MovieCarouselComponent {
+export class MovieCarouselComponent implements OnInit, AfterViewInit{
 
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef
   constructor () {}
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
 
   ngOnInit(){
+  }
 
+  private initSwiper(){
+    return new Swiper(this.swiperContainer.nativeElement,{
+      slidesPerView: 3,
+      slidesPerGroup: 2,
+      centeredSlides: true,
+      loop: true,
+      breakpoints:{
+        600:{
+          slidesPerView: 3,
+          slidesPerGroup: 2,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        900:{
+          slidesPerView: 3,
+          slidesPerGroup: 2,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        1200:{
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1500:{
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1800:{
+          slidesPerView: 5,
+          slidesPerGroup: 6,
+          spaceBetween: 5,
+          centeredSlides: false,
+        }
+      }
+    })
   }
 
 }
